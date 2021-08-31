e17ff849de7b9c7bf87770066afc1454369d87ce
facubrusa <facundobrusa11@gmail.com>
2021-08-30 22:38:28 -0300

    strapi with cms

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4d3726ee88c2486af61c7316eb6ecd12'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '72b2fda7-e4fe-4bb2-9661-8d3e2d7f9c48'
+};
